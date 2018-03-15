37e4d27668e3698b1c8645fc4c49f5b7cdf0ac2f
Mike Heiser <mikeheiser@Mikes-MacBook-Air.local>
2018-03-15 12:41:14 -0700

    started cloze card logic

--- clozeCard.js
+++ clozeCard.js
@@ -11,17 +11,12 @@ var ClozeCard = function(text, cloze) {
     //function that returns the full text replaced with a blank for cloze card and pushes into array
     this.part = function() {
         if (this.text.includes(this.cloze)) {
-            return this.text.replace(this.cloze, + "__________");
+            this.text.replace(this.cloze, + "__________");
     //else inform user there was an error displaying the card 
         } else {
             console.log("Error displaying card");
         }
-            //ask user to enter answer
-            //store user answer in a var
-            //compare user answer to correct answer
-            //if match consolelog that was correct
-            //if wrong consolelog that was wrong
-            //give option to move to next card or return to card menu (basic, cloze)
+
     }
 }
 
@@ -35,11 +30,11 @@ var lethalWeaponBasic = new BasicCard("What movie did the gang make a sequel to?
     // console.log(BasicCardHolder);
 
 // cloze card cards
-var milkSteak = new ClozeCard("Milk Steak is Charlie Kelly's favorite food", "Milk Steak");
+var milkSteak = new ClozeCard("Milk Steak is Charlie Kelly's favorite food", "milk steak");
     clozeCardHolder.push(milkSteak);
-var whoPooped = new ClozeCard("Frank pooped the bed!", "Frank");
+var whoPooped = new ClozeCard("Frank pooped the bed!", "frank");
     clozeCardHolder.push(whoPooped);
-var lethalWeapon = new ClozeCard("The gang produced the 6th installation in the Leathal Weapon action movie franchise.", "Leathal Weapon");
+var lethalWeapon = new ClozeCard("The gang produced the 6th installation in the Leathal Weapon action movie franchise.", "leathal weapon");
     clozeCardHolder.push(lethalWeapon);
     // console.log(clozeCardHolder);
 
--- main.js
+++ main.js
@@ -4,7 +4,7 @@ var confirm = require('inquirer-confirm');
 
 
 //game logic
-    inquirer.prompt([{
+inquirer.prompt([{
     type: "list",
     name: "cards",
     message: "What kind of cards would you like to use?",
@@ -17,74 +17,109 @@ var confirm = require('inquirer-confirm');
         console.log("-----------------------------------------------")
         console.log(clozeCard.BasicCardHolder[0].front);
         console.log("-----------------------------------------------")
-    confirm('View Back of Card?')
-        .then(function confirmed() {
-            console.log("-----------------------------------------------")
-            console.log(clozeCard.BasicCardHolder[0].back);
-            console.log("-----------------------------------------------")
+        confirm('View Back of Card?')
+            .then(function confirmed() {
+                console.log("-----------------------------------------------")
+                console.log(clozeCard.BasicCardHolder[0].back);
+                console.log("-----------------------------------------------")
+                confirm('View Next of Card?')
+                    .then(function confirmed() {
+                            console.log("-----------------------------------------------")
+                            console.log(clozeCard.BasicCardHolder[1].front);
+                            console.log("-----------------------------------------------")
+                            confirm('View Back of Card?')
+                                .then(function confirmed() {
+                                        console.log("-----------------------------------------------")
+                                        console.log(clozeCard.BasicCardHolder[1].back);
+                                        console.log("-----------------------------------------------")
+                                        confirm('View Next of Card?')
+                                            .then(function confirmed() {
+                                                    console.log("-----------------------------------------------")
+                                                    console.log(clozeCard.BasicCardHolder[2].front);
+                                                    console.log("-----------------------------------------------")
+                                                    confirm('View Back of Card?')
+                                                        .then(function confirmed() {
+                                                                console.log("-----------------------------------------------")
+                                                                console.log(clozeCard.BasicCardHolder[2].back);
+                                                                console.log("-----------------------------------------------")
+
+                                                            },
+
+                                                            function cancelled() {
+                                                                console.log('OOPS ERROR');
+
+                                                            });
+
+                                                },
+
+                                                function cancelled() {
+                                                    console.log('OOPS ERROR');
+
+
+                                                });
+                                    },
+
+                                    function cancelled() {
+                                        console.log('OOPS ERROR');
+
+
+                                    });
+
+                        },
+
+                        function cancelled() {
+                            console.log('OOPS ERROR');
+
+                        });
+
+            }, function cancelled() {
+                console.log('OOPS ERROR');
+
+            });
+
+    }
+    if (command.cards === "Cloze-Cards") {
+        console.log("-----------------------------------------------")
+        console.log(clozeCard.clozeCardHolder[0].text);
+        console.log("-----------------------------------------------")
+        inquirer.prompt([{
+            type: "input",
+            name: "cards",
+            message: "What is the missing word?"
+        }]).then(function (command) {
+            if (command.name === this.cloze) {
+                console.log("That is correct!");
+            } else {
+                console.log("That is incorrect the correct answer is " + this.clozeCard);
+            }
             confirm('View Next of Card?')
                 .then(function confirmed() {
                         console.log("-----------------------------------------------")
-                        console.log(clozeCard.BasicCardHolder[1].front);
+                        console.log(clozeCard.clozeCardHolder[1].text);
                         console.log("-----------------------------------------------")
-                        confirm('View Back of Card?')
-                            .then(function confirmed() {
-                                    console.log("-----------------------------------------------")
-                                    console.log(clozeCard.BasicCardHolder[1].back);
-                                    console.log("-----------------------------------------------")
-                                    confirm('View Next of Card?')
-                                        .then(function confirmed() {
-                                                console.log("-----------------------------------------------")
-                                                console.log(clozeCard.BasicCardHolder[2].front);
-                                                console.log("-----------------------------------------------")
-                                                confirm('View Back of Card?')
-                                                    .then(function confirmed() {
-                                                            console.log("-----------------------------------------------")
-                                                            console.log(clozeCard.BasicCardHolder[2].back);
-                                                            console.log("-----------------------------------------------")
-
-                                                        },
-
-                                                        function cancelled() {
-                                                            console.log('OOPS ERROR');
-                                            
-                                                        });
-
-                                            },
-
-                                            function cancelled() {
-                                                console.log('OOPS ERROR');
-                                
-
-                                            });
-                                },
-
-                                function cancelled() {
-                                    console.log('OOPS ERROR');
-                    
-
-                                });
-
                     },
 
                     function cancelled() {
                         console.log('OOPS ERROR');
-        
+
                     });
 
-        }, function cancelled() {
-            console.log('OOPS ERROR');
-
-        });
+        })
 
     }
-    if (command.cards === "Cloze-Cards"){
-        console.log("-----------------------------------------------")
-        console.log(clozeCard.clozeCardHolder[0].text);
-        console.log("-----------------------------------------------")
 
-
-
-    }
 
 });
+
+// var questions = [
+//         {
+//         type: input,
+//         name: answer,
+//         message: "What is the missing word?"
+//     },
+//     {
+//         type: confirm,
+//         name: nextQuestion,
+//         message: "Move onto the next card?"
+//     }
+// ];
